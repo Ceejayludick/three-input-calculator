9adad0b474ebc838024fd4db2b622eff5090d83c
Ceejayludick <ceejayludick13@gmail.com>
2023-01-24 13:42:26 +0200

    Update if.js

--- if.js
+++ if.js
@@ -1,12 +1,1 @@
 
-
-//console.log(`${num1} ${operator} ${num2} = ${result}`);
-console.log(`${num1}${operator}${num2}= ${result}`);
-
-const numberButtons = document.querySelectorAll('[data-number]')
-const operationButtons = document.querySelectorAll('[data-operation]')
-const equalsButton = document.querySelector('[data-equals]')
-const deleteButton = document.querySelector('[data-delete]')
-const allClearButton = document.querySelector('[data-all-clear]')
-const previousOperandTextElement = document.querySelector('[data-previous-operand]')
-const currentOperandTextElement = document.querySelector('[data-current-operand]')
